88893d5ab66c8c266b472c331b922d337aa6581f
midmiako <mayomi@baka.wang>
2023-08-14 13:15:05 +0800

    test

---
+++ script/nf_rank.py
@@ -0,0 +1,44 @@
+import json
+import requests
+
+
+def Countryconvert(code):
+    with open("../countries.json", 'r') as r:
+        j = json.loads(r.content.decode("utf-8"))
+        for i in j:
+            if code == i['iso2']:
+                return i['translations']['cn'], i['emoji']
+
+
+def exec():
+    with open("../netflix.json", 'r') as r:
+        j = json.loads(r.content.decode("utf-8"))
+        countrylist = []
+        for i in j:
+            if i['Code'] != 'RU':
+                c, e = Countryconvert(i['Code'])
+                tax = 1
+                if i['Code'] == 'AR':
+                    tax = 1.64
+                content = {
+                    'country': c,
+                    'emoji': e,
+                    'Code': i['Code'],
+                    'Currency': i['Currency'],
+                    'Basic': round(float(i['Basic'])*tax, 2),
+                    'Standard': round(float(i['Standard'])*tax, 2),
+                    'Premium': round(float(i['Premium'])*tax, 2),
+                    'BasicCNY': round(float(i['BasicCNY'])*tax, 2),
+                    'StandardCNY': round(float(i['StandardCNY'])*tax, 2),
+                    'PremiumCNY': round(float(i['PremiumCNY'])*tax, 2),
+                }
+                countrylist.append(content)
+        countrylist = sorted(countrylist, key=lambda i: i['PremiumCNY'])
+        jsObj = json.dumps(countrylist)
+
+        fileObject = open('api_ranking.json', 'w')
+        fileObject.write(jsObj)
+        fileObject.close()
+
+
+exec()
