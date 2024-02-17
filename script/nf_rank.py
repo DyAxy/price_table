8241988fd240ef1522c73087b27be747b6763bd7
Dy Haruka <111729065+DyAxy@users.noreply.github.com>
2024-02-17 16:36:37 +0800

    Update nf_rank.py

--- script/nf_rank.py
+++ script/nf_rank.py
@@ -12,28 +12,14 @@ def Countryconvert(code):
 def exec():
     with open("../netflix.json", 'r', encoding="utf-8") as r:
         j = json.loads(r.read())
-        countrylist = []
+        countryList = {}
         for i in j:
-            if i['Currency'] != '':
-                c, e = Countryconvert(i['Code'])
-                tax = 1
-                if i['Code'] == 'AR':
-                    tax = 1.64
-                content = {
-                    'country': c,
-                    'emoji': e,
-                    'Code': i['Code'],
-                    'Currency': i['Currency'],
-                    'Basic': round(float(i['Basic'])*tax, 2),
-                    'Standard': round(float(i['Standard'])*tax, 2),
-                    'Premium': round(float(i['Premium'])*tax, 2),
-                    'BasicCNY': round(float(i['BasicCNY'])*tax, 2),
-                    'StandardCNY': round(float(i['StandardCNY'])*tax, 2),
-                    'PremiumCNY': round(float(i['PremiumCNY'])*tax, 2),
-                }
-                countrylist.append(content)
-        countrylist = sorted(countrylist, key=lambda i: i['PremiumCNY'])
-        jsObj = json.dumps(countrylist)
+            countryList[i['Code']] = i
+            c,e = Countryconvert(i['Code'])
+            countryList[i['Code']]['country'] = c
+            countryList[i['Code']]['emoji'] = e
+
+        jsObj = json.dumps(countryList)
 
         fileObject = open('api_ranking.json', 'w', encoding='utf-8')
         fileObject.write(jsObj)
